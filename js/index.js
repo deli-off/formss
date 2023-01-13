a92c0f73ccf42d9ad0ca55c9f71ce52a53e9f2b7
deli-off <dzamsedovnazif1@gmail.com>
2023-01-13 13:10:18 +0500

    finished

--- js/index.js
+++ js/index.js
@@ -4,18 +4,16 @@ let errorIcon = document.querySelector('.icon')
 exampleForm.onsubmit = (event) => {
     let user = {}
     let fm = new FormData(exampleForm)
+    event.preventDefault()
 
     fm.forEach((value, key) => {
         user[key] = value
+        const myInpit = document.querySelector('.required')
+        if (myInpit.value === '') {
+            errorIcon.style.display = 'block'
+        }
     })
 
     console.log(user)
 }
 
-function requiredInput() {
-    exampleForm.onsubmit = () => {
-        const myInpit = document.querySelector('.required')
-        myInpit.setAttribute('required', 'required')
-        errorIcon.getElementsByClassName.display = 'block'
-    }
-}
